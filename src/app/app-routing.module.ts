2a73609fc4b7ccc82bad957e989dffd7e95b4c84
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-11-27 15:21:47 -0400

    header listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { VentasComponent } from './pages/ventas/ventas.component';
+import { MaterialesComponent } from './pages/materiales/materiales.component';
+import { ProveedoresComponent } from './pages/proveedores/proveedores.component';
+import { PedidosComponent } from './pages/pedidos/pedidos.component';
+import { EmpleadosComponent } from './pages/empleados/empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cliente', component: ClientesComponent },
+  { path: 'venta', component: VentasComponent },
+  { path: 'material', component: MaterialesComponent },
+  { path: 'proveedor', component: ProveedoresComponent },
+  { path: 'pedido', component: PedidosComponent },
+  { path: 'empleado', component: EmpleadosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'cliente' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { VentasComponent } from './pages/ventas/ventas.component';
+import { MaterialesComponent } from './pages/materiales/materiales.component';
+import { ProveedoresComponent } from './pages/proveedores/proveedores.component';
+import { PedidosComponent } from './pages/pedidos/pedidos.component';
+import { EmpleadosComponent } from './pages/empleados/empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ClientesComponent,
+    VentasComponent,
+    MaterialesComponent,
+    ProveedoresComponent,
+    PedidosComponent,
+    EmpleadosComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
