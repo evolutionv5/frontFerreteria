2dc076aa27e171249eb45cfaf5ff4d47152f3b69
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-11-27 21:11:40 -0400

    bug henry ñ corregido

--- src/app/pages/empleados/empleados.component.ts
+++ src/app/pages/empleados/empleados.component.ts
@@ -4,24 +4,28 @@ import { EmpleadoService } from '../../services/empleado.service';
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
-  styleUrls: ['./empleados.component.scss']
+  styleUrls: ['./empleados.component.scss'],
 })
 export class EmpleadosComponent implements OnInit {
+  constructor(private serviceempleado: EmpleadoService) {}
 
-  constructor(private serviceempleado: EmpleadoService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-
-  añadirEmpleado(){
-    var emp = {ci: '77346', name: 'roger', apPaterno: 'chura',apMaterno: 'siñani', direccion: 'aca nomas', telefono: '1254', correo: 'ro@gmail.com'};
+  anadirEmpleado() {
+    var emp = {
+      ci: '77346',
+      name: 'roger',
+      apPaterno: 'chura',
+      apMaterno: 'siñani',
+      direccion: 'aca nomas',
+      telefono: '1254',
+      correo: 'ro@gmail.com',
+    };
     this.serviceempleado.addEmpleado(emp).subscribe(
       (response) => {
-          console.log(response);
+        console.log(response);
       },
-      (er) => console.log(er),
-  );
+      (er) => console.log(er)
+    );
   }
-
 }
