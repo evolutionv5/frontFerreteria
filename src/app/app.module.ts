ac2548ef5775fd5a475ea731bf967775aad3b402
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-11-27 21:29:50 -0400

    bug henry corregido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -27,7 +28,7 @@ import { HomeComponent } from './pages/home/home.component';
     EmpleadosComponent,
     HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
