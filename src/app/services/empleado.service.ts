d07282a80becbf1ad22b0c86e4ea041335833f31
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-11-27 21:26:59 -0400

    bug 2

--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -4,44 +4,46 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmpleadoService {
-
-  url = 'http://localhost:3000/api/';
+  public url: string;
 
   constructor(private http: HttpClient) {
-    
+    this.url = 'http://localhost:3000/api/';
   }
 
   // Http Headers
   httpOptions = {
     headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-    })
+      'Content-Type': 'application/json',
+    }),
   };
 
   addEmpleado(sala): Observable<any> {
-    return this.http.post<any>(this.url + 'addEmpleado', sala, this.httpOptions).pipe(retry(1), catchError(this.errorHandl));
+    return this.http
+      .post<any>(this.url + 'addEmpleado', sala, this.httpOptions)
+      .pipe(retry(1), catchError(this.errorHandl));
   }
 
   listarEmpleados(): Observable<any> {
-    return this.http.get<any>(this.url + 'findAllSala').pipe(retry(1), catchError(this.errorHandl));
+    return this.http
+      .get<any>(this.url + 'findAllSala')
+      .pipe(retry(1), catchError(this.errorHandl));
   }
 
   // Error handling
-  errorHandl(error) {
+  errorHandl(error): any {
     let errorMessage = '';
     if (error.error instanceof ErrorEvent) {
-        // Get client-side error
-        errorMessage = error.error.message;
+      // Get client-side error
+      errorMessage = error.error.message;
     } else {
-        // Get server-side error
-        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
     console.log(errorMessage);
     return throwError(errorMessage);
-}
+  }
 }
