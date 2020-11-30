404f638ea909ddb97329e7441c83f30485a1b3f1
evolutionv5 <jorgeluiscastrov5@gmail.com>
2020-11-30 15:01:22 -0400

    servicios de listado en ventas listo

--- src/app/pages/ventas/ventas.component.ts
+++ src/app/pages/ventas/ventas.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto, Venta } from '../../models/interfaces';
+import { ProductoService } from '../../services/producto.service';
+import { VentaService } from '../../services/venta.service';
 
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
-  styleUrls: ['./ventas.component.scss']
+  styleUrls: ['./ventas.component.scss'],
 })
 export class VentasComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public productos: Producto[];
+  public ventas: Venta[];
+  constructor(
+    public productoService: ProductoService,
+    ventaService: VentaService
+  ) {
+    productoService.getProduct().subscribe((res: Producto[]) => {
+      this.productos = [...res];
+    });
+    ventaService.getSale().subscribe((res: Venta[]) => {
+      this.ventas = [...res];
+    });
   }
 
+  ngOnInit(): void {}
 }
